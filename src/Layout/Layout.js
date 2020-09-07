f4dd13aafaa17b81924462a045a80b4fe5fc9b32
Baldeep Singh <baldeep@pop-os.localdomain>
2020-09-07 07:22:53 +0530

    Navbar 1st

---
+++ src/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Layout.module.css';
+import Navbar from './Navbar/Navbar';
+
+
+
+const NavbarComponent=()=>{
+        return(
+            <Navbar/>
+        );
+}
+
+export default NavbarComponent;
---
+++ src/Layout/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import logo from '../../assets/logo.png'
+import './Navbar.css';
+
+const NavbarComponent=()=>{
+        const navItems=['Work','About','Contact'];
+        const nav=navItems.map(i=> <Nav.Item><Nav.Link className='nav-link font-weight-bold'>{i}/&gt;</Nav.Link> </Nav.Item>);
+        return(
+            <Navbar fixed ='top' collapseOnSelect expand="sm" bg="transparent" variant="dark">
+            <Navbar.Brand href="#home"><img className='navbar-logo' src={logo} alt='logo'/></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className='ml-auto' activeKey="#home">
+              <Nav.Item><Nav.Link className='font-weight-bold' href='#home'>Start/&gt;</Nav.Link> </Nav.Item>
+              {nav}
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        );
+}
+
+export default NavbarComponent;
