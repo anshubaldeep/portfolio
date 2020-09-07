ff8b8b2f0655786ad3eaf529a6a00af2eaaa2a70
anshubaldeep <anshubaldeep@gmail.com>
2020-09-08 03:35:54 +0530

    Jumbotron 1st

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -6,14 +6,14 @@ import './Navbar.css';
 
 const NavbarComponent=()=>{
         const navItems=['Work','About','Contact'];
-        const nav=navItems.map(i=> <Nav.Item><Nav.Link className='nav-link font-weight-bold'>{i}/&gt;</Nav.Link> </Nav.Item>);
+        const nav=navItems.map(i=> <Nav.Item><Nav.Link className='nav-link font-weight-bold custom-nav-link'>{i}/&gt;</Nav.Link> </Nav.Item>);
         return(
             <Navbar fixed ='top' collapseOnSelect expand="sm" bg="transparent" variant="dark">
             <Navbar.Brand href="#home"><img className='navbar-logo' src={logo} alt='logo'/></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className='ml-auto' activeKey="#home">
-              <Nav.Item><Nav.Link className='font-weight-bold' href='#home'>Start/&gt;</Nav.Link> </Nav.Item>
+              <Nav.Item><Nav.Link className='nav-link font-weight-bold custom-nav-link' href='#home'>Start/&gt;</Nav.Link> </Nav.Item>
               {nav}
               </Nav>
             </Navbar.Collapse>
