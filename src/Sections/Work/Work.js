af7ebf514bd949d3d31d9661a4555280a34dfe68
Baldeep Singh <baldeep@pop-os.localdomain>
2020-09-15 07:07:50 +0530

    Work commit 2

--- src/Sections/Work/Work.js
+++ src/Sections/Work/Work.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import img from '../../Assets/logo.png';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Tilt from 'react-tilt';
-import Aux from '../../hoc/Aux';
+
 
 const projects=[
     {
@@ -57,11 +56,11 @@ const WorkSection=()=>{
             return (
                 
                 <Col md={6} sm={12} className="d-flex justify-content-center mt-md-5 mt-2">
-                    <Tilt className="Tilt" options={{ max : 25 , scale: 1.1}} style={{ height: 291, width: 300 }}>
+                    <Tilt className="Tilt" options={{ max : 30 , scale: 1.1}} style={{ height: 291, width: 300 }}>
                         <Card className="bg-transparent text-white text-center ml-md-0 ml-5">
                             <Card.Img src={proj.path} alt={proj.alt} />
-                            <Card.ImgOverlay className="Tilt-inner text-left">
-                                <Card.Title className='font-weight-bold' style={{position:"absolute",bottom:40 , left:-45}}>{proj.name}<hr/></Card.Title>
+                            <Card.ImgOverlay className="text-left">
+                                <Card.Title className='Tilt-inner font-weight-bold' style={{position:"absolute",bottom:40 , left:-45}}>{proj.name}<hr/></Card.Title>
                             </Card.ImgOverlay>
                         </Card>
                     </Tilt>
