928386a84f455a47559ba551921163810942b040
anshubaldeep <anshubaldeep@gmail.com>
2020-10-07 07:24:05 +0530

    MyJourney complete

--- src/Sections/MyJourney/MyJourney.js
+++ src/Sections/MyJourney/MyJourney.js
@@ -1,122 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
-// import Prism from 'prismjs';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
+import MyJourneyCode from './MyJourneyCode';
 
 
 
 const MyJourney=()=>{
-    const code1=`class Baldeep_Singh {
-        ··// I am a technology enthusiast who loves new challenges :)
-        ··// I am always trying to expand the area of my skills
-        ·· constructor() {
-        ····this.name = 'Baldeep Singh'
-        ····this.dateOfBirth = '03-24-1997'
-        ····this.email = 'anshubaldeep@gmail com'
-        ··}
-        ·· workExperience() {
-        ····return [
-        ······{ '2020-now' : 'Full-stack Developer/Owner at Pixel Lab' }
-        ······{ '2020-now' : 'Full-stack Developer at SARE S.A.' }
-        ······{ '2017-2020': 'Front-end Developer at SARE S.A.' }
-        ······{ '2015-2017': 'Full-Stack Designer at SARE S.A.' }
-        ······{ '2013-2015': 'Graphic Designer at SARE S.A.' }
-        ······{ '2006-now' : 'Full-Stack Designer/developer at yasio.pl' }
-        ····]
-        ··}
-        ·· education() {
-        ····return [
-        ······{ '2017-2018': 'Google Developer Challenge Scholarship - Mobile Web Specialist' },
-        ······{ '2012-2014': 'Wyższa Szkoła Biznesu w Dąbrowie Górniczej - mgr IT' },
-        ······{ '2008-2012': 'Wyższa Szkoła Technologii Informatycznych w Katowicach - inż. IT' }
-        ····]
-        ··}
-        ·· skills() {
-        ····return [ 'HTML/CSS/JS', 'Vue', 'Node.js', 'jQuery', 'Bootstrap/Bulma/Material Design', 'Webpack/Gulp/Grunt', 'SASS/Less', 'npm/yarn/bower', 'Docker', 'PWA', 'SSR', 'SPA', 'GIT/CVS', 'Cordova', 'NativeScript', 'Electron', 'Web-extensions', 'Web Sockets', 'Firebase, 'RWD/W3C/ARIA/WCAG', 'XSLT', 'Smarty/Twig', 'PHP', 'MySQL/NoSQL', 'Wordpress', 'Photoshop', 'Illustrator', 'After Effects', 'Premiere', 'Motion design', 'UX/UI', 'DTP', 'C#', 'Unity', 'TypeScript', 'NestJS' `
+    
         
-        const code2=`
-        class Baldeep_Singh {
-            // I am a technology enthusiast who loves new challenges :)
-            // I am always trying to expand the area of my skills
-            constructor() {
-              this.name = 'Baldeep Singh'
-              this.dateOfBirth = '03-24-1997'
-              this.email = 'anshubaldeep@gmail com'
-            }
-            workExperience() {
-              return [
-                { '2020-now' : 'Full-stack Developer/Owner at Pixel Lab' }
-                { '2020-now' : 'Full-stack Developer at SARE S A ' }
-                { '2017-2020': 'Front-end Developer at SARE S A ' }
-                { '2015-2017': 'Full-Stack Designer at SARE S A ' }
-                { '2013-2015': 'Graphic Designer at SARE S A ' }
-                { '2006-now' : 'Full-Stack Designer/developer at yasio pl' }
-              ]
-            }
-            education() {
-              return [
-                { '2017-2018': 'Google Developer Challenge Scholarship - Mobile Web Specialist' },
-                { '2012-2014': 'Wyższa Szkoła Biznesu w Dąbrowie Górniczej - mgr IT' },
-                { '2008-2012': 'Wyższa Szkoła Technologii Informatycznych w Katowicach - inż  IT' }
-              ]
-            }
-            skills() {
-              return [ 'HTML/CSS/JS', 'Vue', 'Node js', 'jQuery', 'Bootstrap/Bulma/Material Design', 'Webpack/Gulp/Grunt', 'SASS/Less', 'npm/yarn/bower', 'Docker', 'PWA', 'SSR', 'SPA', 'GIT/CVS', 'Cordova', 'NativeScript', 'Electron', 'Web-extensions', 'Web Sockets', 'Firebase, 'RWD/W3C/ARIA/WCAG', 'XSLT', 'Smarty/Twig', 'PHP', 'MySQL/NoSQL', 'Wordpress', 'Photoshop', 'Illustrator', 'After Effects', 'Premiere', 'Motion design', 'UX/UI', 'DTP', 'C#', 'Unity', 'TypeScript', 'NestJS' `
+    //     const code2=`
+    //     class Baldeep_Singh {
+    //         // I am a technology enthusiast who loves new challenges :)
+    //         // I am always trying to expand the area of my skills
+    //         constructor() {
+    //           this.name = 'Baldeep Singh'
+    //           this.dateOfBirth = '03-24-1997'
+    //           this.email = 'anshubaldeep@gmail com'
+    //         }
+    //         workExperience() {
+    //           return [
+    //             { '2020-now' : 'Software Engineer at Newgen Software' }
+    //             { '2019': 'Web Developer, Intern at Lex Bolster ' }
+    //             { '2018-2019': 'Junior Application Consultant at SIE Services ' }
+    //             { '2019': 'Front End Developer, Intern at Aeris Technologies ' }
+    //             { '2018' : 'Blockchain Developer, Intern at DXC Technologies ' }
+    //           ]
+    //         }
+    //         education() {
+    //           return [
+    //             { '2016-2020': 'B.E. in Information Technology' },
+    //             { '2014-2015': 'HSC from Sneh International School' },
+    //           ]
+    //         }
+    //         skills() {
+    //           return [ 'HTML/CSS/JS', 'ReactJS', 'NodeJS', 'jQuery', 'Bootstrap/Material Design', 'Webpack/Gulp', 'SASS/Less', 'npm/yarn',  'GIT', 'Firebase,  'MySQL/NoSQL',  'Photoshop',  'UX/UI',  'C++', 'Python', 'Blockchain', 'NextJS'  `
        
-    let code=code1;
-    const getWidth = () => window.innerWidth 
-  || document.documentElement.clientWidth 
-  || document.body.clientWidth;
+    // let code=code2;
+    
 
-function useCurrentWidth() {
-  let [width, setWidth] = useState(getWidth());
+  
 
-  useEffect(() => {
-    let timeoutId = null;
-    const resizeListener = () => {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => setWidth(getWidth()), 150);
-    };
-    window.addEventListener('resize', resizeListener);
 
-    return () => {
-      window.removeEventListener('resize', resizeListener);
-    }
-  }, [])
-  return width;
-}
- let width=useCurrentWidth();
- if(width<768){
-     code=code2;
- }       
 
- const code3=` ]
- ··}
- }`
+ 
+
+
 
         return(
             <Container fluid className='MyJourney'>
                 <Container className='line-numbers ' id='myjourney'>
                 <p className='section-start mt-5'>MyJourney/&gt;</p>
-                <SyntaxHighlighter 
-                    language="javascript" 
-                    showLineNumbers showInlineLineNumbers
-                    style={tomorrow} 
-                    lineProps={{style: {wordBreak: 'break-all', whiteSpace: 'pre-wrap'}}} 
-                    wrapLines={true}
-                    >
-                {/* <pre className='language-javascript code-wrp line-numbers'>
-                    <code className='language-javascript'> */}
-                        {code}
-                    {/* </code>  */}
-                        <span className="react-rotating-text-cursor">|</span> 
-                    {/* <code> */}
-                        {code3}
-                    {/* </code>
-                </pre> */}
-                </SyntaxHighlighter>
+                <MyJourneyCode/>
                 </Container>
             </Container>
         );
---
+++ src/Themes/dark-theme.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './dark-theme.scss';
+
+const Theme = () => (<React.Fragment></React.Fragment>);
+
+export default Theme;
